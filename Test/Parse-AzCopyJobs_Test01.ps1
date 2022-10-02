265c0dc57117fc0c054b1ea7da9ff5ee56296848
Matti Pedersen <mrmattipants@gmail.com>
2022-10-01 22:09:16 -0500

    Uploaded "Parse-AzCopyJobs_Test0x.,ps1" Scripts
    Uploaded "Parse-AzCopyJobs_Test01.ps1", "Parse-AzCopyJobs_Test02.ps1" & "Parse-AzCopyJobs_Test03.ps1" Scripts, which were Written for the purpose of Testing the "AzCopy Jobs List" Command Parser, etc.

---
+++ Test/Parse-AzCopyJobs_Test01.ps1
@@ -0,0 +1,35 @@
+﻿
+Remove-Variable * -ErrorAction SilentlyContinue
+Remove-Module *
+$error.Clear()
+
+$JobId = @()
+$StartTime = @()
+$Status = @()
+$Command = @()
+
+$LastAzCopyJob = (C:\AzCopy\azcopy.exe jobs list)
+
+$JobId = ($LastAzCopyJob | Where-Object {$_ -match 'JobId:'} | Foreach-Object {$_ -replace 'JobId: ',''})
+$StartTime = ($LastAzCopyJob | Where-Object {$_ -match 'Start Time:'} | Foreach-Object {$_ -replace 'Start Time: ',''})
+$Statuses = ($LastAzCopyJob | Where-Object {$_ -match 'Status:'} | Foreach-Object {$_ -replace 'Status: ',''})
+$Commands = ($LastAzCopyJob | Where-Object {$_ -match 'Command:'} | Foreach-Object {$_ -replace 'Command: ',''})
+
+[int]$max = $JobId.Count
+$Jobs = for ( $i = 0; $i -lt $max; $i++) {
+    [pscustomobject] @{
+        JobId = $JobId[$i]
+        StartTime = $StartTime[$i]
+        Status = $Statuses[$i]
+        Command = $Commands[$i]
+    }
+}
+
+$Jobs | Format-Table
+
+$Jobs | Select-Object -First 1 | Format-List
+
+Write-Host "JobId: $($Jobs.JobId[0])"
+Write-Host "Start Time: $($Jobs.StartTime[0])"
+Write-Host "Status: $($Jobs.Status[0])"
+Write-Host "Command: $($Jobs.Command[0])"
